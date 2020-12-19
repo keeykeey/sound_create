9ccf8e17ed7a55fb7f7efe833d5144a6e8dee641
keeykeey <sh1n_fuj1to@icloud.com>
2020-12-20 01:05:48 +0900

    login form

--- frontend/src/components/Login/LoginControl.js
+++ frontend/src/components/Login/LoginControl.js
@@ -1,93 +1,127 @@
-import React, {useState,useEffect} from 'react'
-import axios from 'axios'
-import Style from './LoginControl.module.scss'
-import User from './User'
+import React, {useState,useRef} from 'react';
+import Style from './LoginControl.module.scss';
+import AuthService from '../../services/auth.service.js';
+import Form from 'react-validation/build/form';
+import {withRouter} from 'react-router-dom';//to enable props.history.push
 
-const LoginControl = ()=> {
-  const [drfUserInfo,setDrfUserInfo] = useState([])
-  useEffect(() => {
-      axios.get('http://127.0.0.1:8000/user/drfcustomuser',{
-          headers:{
-              'Authorization':'Token 686f3dcae81cfe82ec6c84fc8f2ab0a953a9fd05'
-          }
-      })
-      .then(res => {setDrfUserInfo(res.data);});
-  },[]);
-
-  const [inputUsername,setInputUsername] = useState([])
-  const [inputPassword,setInputPassword] = useState([])
-
-
-  {/*const getUsername = (id) => {
-    axios.get(`http://localhost:8000/drfPostSong/${id}`,{
-        headers:{
-            'Authorization':'Token 686f3dcae81cfe82ec6c84fc8f2ab0a953a9fd05'
-        }})
-    .then(res => {setInputUsername(res.data)})
-  }*/}
-
-  const handleInputUsername = () => event => {
-    const value = event.target.value;
-    setInputUsername(value)
+const required = (value) => {
+  if (!value){
+    return (
+      <div role='alert'>
+        this field is required!
+      </div>
+    );
   }
+};
 
-  const handleInputPassword = () => event => {
-    const value = event.target.value;
-    setInputPassword(value)
-  }
+const LoginControl = (props) => {
+  const form = useRef();
+  const checkBtn = useRef();
 
-  const clickLoginButton = async () => {
-    try{
-      await User.login(inputUsername,inputPassword);
-      console.log('success')
-      window.history.push({pathname:'/'})
-    }catch(e){
-      console.log('error')
-    }
+  const [username,setUsername] = useState('');
+  const [password,setPassword] = useState('');
+  const [loading,setLoading] = useState(false);
+  const [message,setMessage] = useState('');
+
+  const onChangeUsername = (event) => {
+    const username = event.target.value;
+    console.log('username:',username)
+    setUsername(username);
   };
 
+  const onChangePassword = (event) => {
+    const password = event.target.value;
+    console.log('password:',password)
+    setPassword(password);
+  }
 
+  const handleLogin = (event) => {
+    event.preventDefault();
+    setMessage('');
+    setLoading(true);
+
+    form.current.validateAll();
+
+    if (checkBtn | 1*0 === 0){
+      AuthService.login(username,password).then(
+        () => {
+          props.history.push('/');
+          window.location.reload();
+        },
+        (error) => {
+          console.log('error...')
+          const resMessage =
+            (error.response &&
+               error.response.data &&
+               error.response.data.message) ||
+             error.message ||
+             error.toString();
+          setLoading(false);
+          setMessage(resMessage);
+          alert('認証に失敗しました。名前とパスワードを確認して下さい。');
+        }
+      );
+    }else{
+      setLoading(false);
+    }
+  };
 
   return(
     <div>
       <div className={Style.login_form_area}>
         <h3 className={Style.title}>
-          Login
+          Please Login
         </h3>
-        <form method='post'>
+        <Form onSubmit={handleLogin} ref={form}>
           <div className={Style.locate_center}>
           <label>
-            <input className={Style.form_content} type='text'
-             onChange={handleInputUsername()} name='username' placeholder='username' />
-
-            <input className={Style.form_content} type='text'
-             onChange={handleInputPassword()} name='password' placeholder='password' />
+            <input
+              className={Style.form_content}
+              type='text'
+              onChange={onChangeUsername}
+              validations = {[required]}
+              name='username'
+              placeholder='username'
+            />
+            <input
+              className={Style.form_content}
+              type='password'
+              onChange={onChangePassword}
+              validations = {[required]}
+              name='password'
+              placeholder='password'
+            />
           </label>
           <p/>
-            <button className={Style.button} type='button' onClick={clickLoginButton()}>
-              ログインする<span/> <i className="fas fa-sign-in-alt"></i>
+            <button className={Style.button} disabled={loading} >
+              {loading}
+              <span>
+                ログインする
+              </span>
+              <i className="fas fa-sign-in-alt"></i>
             </button>
             <p className={Style.description}>
-              ログインすることで<a href=''>利用規約</a>に同意するものとみなされます。
+              ログインすることで<a href='/'>利用規約</a>に同意するものとみなされます。
             </p>
           </div>
-        </form>
+        </Form>
       </div>
 
       <div className={Style.path_to_register}>
         <p className={Style.description_to_register}>
           初めてのご利用ですか？
         </p>
-        <form>
-          <a href=''><button className={Style.button_to_register} type='submit'>
-            アカウントを作成する<span/><i class="fas fa-user-plus"></i>
+        <Form>
+          <a href='/'><button className={Style.button_to_register} type='submit'>
+            アカウントを作成する<span/><i className="fas fa-user-plus"></i>
           </button></a>
-        </form>
+        </Form>
       </div>
     </div>
   );
-}
-export default LoginControl
+};
+
+export default withRouter(LoginControl)
 
 
 
