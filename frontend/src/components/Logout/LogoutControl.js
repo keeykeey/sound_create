171d253cfa0f6fd3f932d66182039e75c8996ff0
keeykeey <sh1n_fuj1to@icloud.com>
2020-12-20 00:59:35 +0900

    logout button

---
+++ frontend/src/components/Logout/LogoutControl.js
@@ -0,0 +1,27 @@
+import React, {useState,useRef} from 'react';
+import Style from './LogoutControl.module.scss';
+import AuthService from '../../services/auth.service.js';
+import Form from 'react-validation/build/form';
+import {withRouter} from 'react-router-dom';//to enable props.history.push
+
+const LogoutControl = (props) => {
+  const handleLogout = (event) => {
+    event.preventDefault();
+    AuthService.logout()
+    props.history.push('/');
+    window.location.reload();
+  }
+
+  return(
+    <Form onSubmit={handleLogout} >
+      <button className={Style.button}>
+        <span>
+          logout
+        </span><br/>
+        <i className="fas fa-sign-out-alt"></i>
+      </button>
+    </Form>
+  )
+}
+
+export default withRouter(LogoutControl)
