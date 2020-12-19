5d2022015d2a2cbd0255f8afea0e147ac9dd4d10
keeykeey <sh1n_fuj1to@icloud.com>
2020-12-20 01:04:03 +0900

    another header components

---
+++ frontend/src/components/Header/SimpleHeader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Style from './SimpleHeader.module.scss'
+
+const SimpleHeader = () => {
+  return(
+    <div>
+      <h2 className={Style.top}>Sound_Create</h2>
+    </div>
+  )
+}
+
+export default SimpleHeader
