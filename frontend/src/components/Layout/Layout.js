6035f7f3cc83ad6a6e1821a2a097b5e75e0d68c4
keeykeey <sh1n_fuj1to@icloud.com>
2020-12-28 22:37:37 +0900

    routing for a APA

--- frontend/src/components/Layout/Layout.js
+++ frontend/src/components/Layout/Layout.js
@@ -5,41 +5,47 @@ import Sidebar from '../Sidebar/Sidebar'
 import MainContents from '../MainContents/MainContents'
 import AuthService from '../../services/auth.service'
 
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import SwitchMainContents from './SwitchMainContents'
+
 const loginName = localStorage.getItem('username')
 const loginId = localStorage.getItem('user_id');
 
 console.log('localstorage',localStorage)
 
-const MypageLayout = ({children})=>{
+const HomeLayout = (props)=>{
   return(
     <div>
       <div>
         <Header/>
       </div>
       <div className = {Style.row}>
-        <div className={Style.leftSide}>
-          <Sidebar/>
-        </div>
-        <div className={Style.rightSide}>
-          <MainContents.Mypage loginName={loginName} loginId={loginId}/>
-        </div>
+        <Router>
+          <div className={Style.leftSide}>
+            <Sidebar/>
+          </div>
+          <div className={Style.rightSide}>
+            <Switch>
+              <Route path='/uprising/'>
+                <MainContents.Uprising/>
+              </Route>
+              <Route path='/famous/'>
+                <MainContents.Famous/>
+              </Route>
+              <Route path='/mypage/'>
+                <MainContents.Mypage loginName={loginName} loginId={loginId}/>
+              </Route>
+              <Route component = {MainContents.Public}/>
+            </Switch>
+          </div>
+        </Router>
       </div>
     </div>
   )
 }
 
-
-const MysongLayout = (props) => {
-  return(
-    <div>
-      mysong layout
-    </div>
-  )
-}
-
 const Layout = {
-  MypageLayout,
-  MysongLayout,
+  HomeLayout,
 }
 
 export default Layout;
--- frontend/src/components/Sidebar/Sidebar.js
+++ frontend/src/components/Sidebar/Sidebar.js
@@ -1,14 +1,45 @@
 import React from 'react'
 import Style from './Sidebar.module.scss'
+import LogoutControl from '../Logout/LogoutControl'
+import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
 
 const Sidebar = (props) => {
 
   return(
       <div>
-        <button className={Style.button} id='button1'>ホーム</button>
-        <button className={Style.button} id='button2'> 急上昇 </button>
-        <button className={Style.button} id='button3'> 人気 </button>
-        <button className={Style.button} id='button4'> マイページ </button>
+        <div className={Style.title}> ページ</div>
+          <Link to='/'>
+            <button className={Style.button} >ホーム/</button>
+          </Link>
+          <Link to='/uprising/'>
+            <button className={Style.button} > 急上昇 uprising</button>
+          </Link>
+          <Link to='famous'>
+            <button className={Style.button} > 人気 famous</button>
+          </Link>
+          <Link to = '/mypage'>
+            <button className={Style.button} > マイページ mypage</button>
+          </Link>
+
+        <div className={Style.title}>お気に入り</div>
+        <div className={Style.block}>
+          <button className={Style.button} > Ray Charls </button>
+          <button className={Style.button} > Mickel Jackson </button>
+          <button className={Style.button} > billy joel </button>
+          <button className={Style.button} > ITOSHIN TV </button>
+          <button className={Style.button} > 将棋の森 </button>
+          <button className={Style.button} > jackson 5 </button>
+          <button className={Style.button} > data science bowl </button>
+          <button className={Style.button} > Bird Watch TV </button>
+          <button className={Style.button} > animal planet </button>
+          <button className={Style.button} > friends </button>
+          <button className={Style.button} > つんく </button>
+          <button className={Style.button} > britony spears </button>
+          <button className={Style.button} > amazon prime </button>
+        </div>
+        <div className={Style.logout}>
+          <LogoutControl/>
+        </div>
       </div>
   )
 }
