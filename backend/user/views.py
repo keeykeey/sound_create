fd3179f853fb06c2969c81d223a77eed5b9f17f4
keeykeey <sh1n_fuj1to@icloud.com>
2020-12-30 00:03:24 +0900

    cleaned up code for django-rest-framework api

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,88 +1,14 @@
-#https://docs.djangoproject.com/ja/3.1/topics/class-based-views/generic-editing/
+'''django rest framework'''
+from rest_framework import viewsets
+from .models import CustomUser, UserRelations
+from .serializers import CustomUserSerializer, UserRelationsSerializer
+from .permissions import ProfilePermission
 
-from django.shortcuts import render,redirect
-from django.contrib.auth.views import LoginView as AuthLoginView
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render , redirect
-from django.urls import reverse
-from django.views import View
-from django.views.generic import CreateView,ListView,DetailView
-from django.contrib.auth.models import User
-from django.contrib.auth import login as auth_login,logout as auth_logout, authenticate
-from .forms import UserCreateForm,LoginForm,TestPostForm#PostSongForm,
-from django.db import models
-from mystudio.models import PostedSong,TestPost
+class CustomUserViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+    permission_classes = (ProfilePermission,)
 
-#for register
-class Register(CreateView):
-    def post(self, request, *args, **kwargs):
-        form = UserCreateForm(data=request.POST)
-        content_dict = {
-            'form':form,
-            'errorMessage':'Invalid username.',
-        }
-        if not form.is_valid():
-            return render(request,'register.html',content_dict)
-
-        form.save()
-        username = form.cleaned_data.get('username')
-        password = form.cleaned_data.get('password1')
-        user = authenticate(username=username, password=password)
-        auth_login(request, user)
-        return redirect('/')
-
-    def get(self, request, *args, **kwargs):
-        form = UserCreateForm(request.POST)
-        return  render(request, 'register.html', {'form': form,})
-
-# for login
-class LoginView(AuthLoginView):
-    def get(self,request,*args,**kwargs):
-        '''method for GET request '''
-        context = {
-            'form' :LoginForm()
-            }
-        return render(request,'login.html',context)
-
-    def post(self,request,*args,**kwargs):
-        '''method for POST request'''
-        form = LoginForm(data=request.POST)
-        if not form.is_valid():
-            return render(request,'login.html',{'form':form})
-        user = form.get_user()
-        auth_login(request,user)
-        return redirect('public')
-
-# for logout
-class LogoutView(AuthLoginView):
-    def get(self,request,*args,**kwargs):
-        auth_logout(request)
-        return render(request,'logout.html')
-
-class TestPostView(CreateView):
-    def post(self,request,*args,**kwargs):
-        form = TestPostForm(request.POST,request.FILES)
-        content_dict={
-            'form':form
-        }
-        if not form.is_valid():
-            print('ooops')
-            return render(request,'test.html')
-        form.save()
-        return redirect('rendertestpost')
-    def get(self,request,*args,**kwargs):
-        form = TestPostForm(data=request.GET)
-        return render(request,'testpost.html',{'form':form,})
-
-class RenderTestPost(DetailView):
-     def get(self,request,*args,**kwargs):
-         object_list = TestPost.objects
-         context = {
-             'object_list':object_list,
-            }
-         return render(request,'testpost.html',context)
-
-     def post(self,request,*args,**kwargs):
-         pass
-
-        #
+class UserRelationsViewSet(viewsets.ModelViewSet):
+    queryset = UserRelations.objects.all()
+    serializer_class = UserRelationsSerializer
