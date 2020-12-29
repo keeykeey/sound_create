94d5a7239ba80a5f172afcc5d6e14983e5423d5d
keeykeey <sh1n_fuj1to@icloud.com>
2020-12-29 21:35:13 +0900

    App's routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,21 +4,20 @@ import Top from './pages/Top'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Auth from './pages/Auth'
+import Home from './pages/Home'
 import Mypage from './pages/Mypage'
-
+import Mysong from './pages/Mysong'
+import Samp1 from './pages/Samp1'
+import Samp2 from './pages/Samp2'
 
 function App() {
   return(
     <Router >
       <Switch>
-        <Route exact path='/' component={Top} />
-        <Route exact path='/login' component={Login} />
-        <Route exact path='/register' component={Register} />
-        <Auth>
-          <Switch>
-            <Route exact path='/mypage' component={Mypage} />
-          </Switch>
-        </Auth>
+        <Route exact path='/top/' component={Top} />
+        <Route exact path='/login/' component={Login} />
+        <Route exact path='/register/' component={Register} />
+        <Route exact path='/' component = {Home} />
       </Switch>
     </Router>
   );
