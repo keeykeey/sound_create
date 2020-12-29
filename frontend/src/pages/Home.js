967cc94bcb28f14e1118e88ca62ddbe20700348c
keeykeey <sh1n_fuj1to@icloud.com>
2020-12-29 21:20:30 +0900

    SPA main page

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+
+const Home = () => {
+    return(
+      <div>
+        <Layout.HomeLayout/>
+      </div>
+    )
+}
+
+export default Home
