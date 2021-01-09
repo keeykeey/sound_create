0b6096aceea78ee615fbe2369bf8ac74f1fcc5c9
keeykeey <sh1n_fuj1to@icloud.com>
2021-01-10 03:46:13 +0900

    cleaned a little

--- frontend/src/components/Layout/Layout.js
+++ frontend/src/components/Layout/Layout.js
@@ -4,6 +4,7 @@ import Header from '../Header/Header'
 import Sidebar from '../Sidebar/Sidebar'
 import MainContents from '../MainContents/MainContents'
 import Login from '../../pages/Login'
+import PostSong from '../PostSong/PostSong'
 import { HashRouter as Router, Route, Switch} from 'react-router-dom'
 
 const loginName = localStorage.getItem('username');
@@ -22,19 +23,25 @@ const HomeLayout = (props)=>{
           </div>
           <div className={Style.rightSide}>
             <Switch>
-              <Route exact path='/uprising/'>
-                <MainContents.Uprising loginName={loginName} loginId={loginId}/>
+              <Route exact path='/user/'>
+                <MainContents.SortByUser loginName={loginName} loginId={loginId}/>
               </Route>
-              <Route exact path='/famous/'>
-                <MainContents.Famous loginName={loginName} loginId={loginId}/>
+              <Route exact path='/genre/'>
+                <MainContents.SortByGenre loginName={loginName} loginId={loginId}/>
               </Route>
               <Route exact path='/login/'>
                 <Login loginName={loginName} loginId={loginId}/>
               </Route>
-              <Route exact path={'/mypage/'+loginName}>
+              <Route exact path={'/mypage/'+loginName+'/'}>
                 <MainContents.Mypage loginName={loginName} loginId={loginId}/>
               </Route>
-              <Route exact path='/favorite/:followeeName' children={<MainContents.FollowingUsersPage />}/>
+              <Route exact path={'/mypage/'+loginName+'/mypost/'}>
+                <PostSong.PostSongPage loginName={loginName} loginId={loginId}/>
+              </Route>
+              <Route exact path='/:followeeName'>
+                <MainContents.EachUsersPage  loginName={loginName} loginId={loginId}/>
+              </Route>
+
               <Route>
                 <MainContents.Public loginName={loginName} loginId={loginId}/>
               </Route>
