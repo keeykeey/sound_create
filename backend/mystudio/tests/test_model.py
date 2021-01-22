bb69f7da811df17c67e75b0ac75828b4d859182a
keeykeey <sh1n_fuj1to@icloud.com>
2021-01-23 01:59:42 +0900

    write up the test for posted-song and like models

--- backend/mystudio/tests/test_model.py
+++ backend/mystudio/tests/test_model.py
@@ -64,7 +64,7 @@ class CreatePostedSongTestCase(TestCase):
             is_public = True,
             genre = 'RO',
             tag = 'tag the song',
-            audio_file = self.audioFile,
+            audio_file = self.audioFileUpLoad,
             posted_day = timezone.datetime.today(),
         )
 
@@ -83,7 +83,7 @@ class CreatePostedSongTestCase(TestCase):
                 is_public = True,
                 genre = 'RO',
                 tag = 'tag the song',
-                audio_file = self.audioFile,
+                audio_file = self.audioFileUpLoad,
                 posted_day = timezone.datetime.today(),
             )
 
@@ -102,7 +102,7 @@ class CreatePostedSongTestCase(TestCase):
                 is_public = True,
                 genre = 'isthissongratherROCKthanPOPS?Idonotknowwhich',
                 tag = 'tag the song',
-                audio_file = self.audioFile,
+                audio_file = self.audioFileUpLoad,
                 posted_day = timezone.datetime.today(),
             )
 
@@ -121,7 +121,7 @@ class CreatePostedSongTestCase(TestCase):
                 is_public = True,
                 genre = 'HH',
                 tag = 'heyyou,justtagmeandyouwontmisswhoIam.heyyou,justtagme...',
-                audio_file = self.audioFile,
+                audio_file = self.audioFileUpLoad,
                 posted_day = timezone.datetime.today(),
             )
 
@@ -131,9 +131,80 @@ class CreatePostedSongTestCase(TestCase):
 
         self.assertEqual(is_valid,False)
 
+class CreateLikesTestCase(TestCase):
+    def setUp(self):
+        #set up user
+        self.USER = CustomUser.objects.create(
+            username = 'testuser',
+            email = 'testuser@email.com',
+            password ='I dont need password'
+        )
+        self.userId = CustomUser.objects.filter(username='testuser')
 
+        #set up song
+        self.song_title = 'testSongTitle'
+        self.audioFile = File(open('mystudio/tests/testAudio.mp3','rb'))
+        self.audioFileUpLoad = SimpleUploadedFile(
+            name = 'testAudio.mp3',
+            content = b'audioFile',
+        )
 
+        self.postedSong = PostedSong.objects.create(
+            user_id=self.USER,
+            song_title = self.song_title,
+            is_public = True,
+            genre = 'GR',
+            tag = 'tag the song',
+            audio_file = self.audioFileUpLoad,
+            posted_day = timezone.datetime.today(),
+        )
 
+    def testCreateLike(self):
+        instance = Likes.objects.create(
+            song_id = self.postedSong,
+            user_id = self.USER,
+            like_casted_day = timezone.datetime.today()
+        )
+
+        #If Id is positive,it means that Like was successfully created
+        self.assertEqual(int(instance.id)>0,True)
+
+    def testCreateLikeWithOutSongId(self):
+        is_valid = False
+        try:
+            instance = Likes.objects.create(
+                song_id = '',
+                user_id = self.USER,
+                like_casted_day = timezone.datetime.today()
+            )
+            is_valid = True
+        except:
+            pass
+        self.assertEqual(is_valid,False)
+
+    def testCreateLikeWithoutUserId(self):
+        is_valid = False
+        try:
+            instance = Likes.objects.create(
+                song_id = self.postedSong,
+                user_id = '',
+                like_casted_day = timezone.datetime.today()
+            )
+            is_valid = True
+        except:
+            pass
+        self.assertEqual(is_valid,False)
+
+    def testCreateLikeWithoutLikeCastedDay(self):
+        is_valid = True
+        try:
+            instance = Likes.objects.create(
+                song_id = self.postedSong,
+                user_id = self.USER,
+            )
+        except:
+            is_valid = False
+        self.assertEqual(is_valid,True)
 
 
 
