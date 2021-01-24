115bcafd14cecc58ac9d84e2b6ec5b8236cb1cf4
keeykeey <sh1n_fuj1to@icloud.com>
2021-01-24 19:11:09 +0900

    deleted unnecessary lines

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Top from './pages/Top'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Home from './pages/Home'
@@ -9,7 +8,6 @@ function App() {
   return(
     <Router >
       <Switch>
-        <Route exact path='/top/' component={Top} />
         <Route exact path='/login/' component={Login} />
         <Route exact path='/register/' component={Register} />
         <Route exact path='/' component = {Home} />
