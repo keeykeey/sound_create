6fce4ddb71b75c89d9fdfe0d148e0d6a88f89ad8
keeykeey <sh1n_fuj1to@icloud.com>
2021-02-05 01:08:32 +0900

    removed unnecessary lines

--- frontend/src/components/PostSong/PostSong.js
+++ frontend/src/components/PostSong/PostSong.js
@@ -28,10 +28,6 @@ const PostSongForm = (props) => {
   const [radioVal,setRadioVal] = useState(false)
   const [genre,setGenre] = useState('')
   const [tag,setTag] = useState('')
-  const [audioFile,setAudioFile]=useState({
-    'name':null,
-    'value':null,
-  })
 
   const handleSongTitleInput = (e)=>{
     setSongTitle(e.target.value)
